fdf9e175042bdd9b11ee7fa710be92cd9c90488b
ErenYeager713 <gourav@accuknox.com>
2022-02-13 16:52:19 +0530

    Checking files in the path

--- tests/main.go
+++ tests/main.go
@@ -1,10 +1,23 @@
 package main
 
 import(
-	"fmt"
+		"fmt"
+    "os"
+    "path/filepath"
 )
 
 func main(){
-	fmt.Println("Hello World")
-}
+	var files []string
 
+    root := "/home/runner/work/vagrants/vagrants"
+    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+        files = append(files, path)
+        return nil
+    })
+    if err != nil {
+        panic(err)
+    }
+    for _, file := range files {
+        fmt.Println(file)
+    }
+}
